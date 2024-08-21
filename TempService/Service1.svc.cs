2c89af0cca80a9a930e5b5dde5c44bfbfd4f043f
sbusiso <sbusisokhotseng@gmail.com>
2024-08-21 12:56:44 +0200

    diddy something

--- TempService/Service1.svc.cs
+++ TempService/Service1.svc.cs
@@ -12,6 +12,10 @@ namespace TempService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        
+        public void imdoingshit()
+        {
+
+            
+        }
     }
 }
